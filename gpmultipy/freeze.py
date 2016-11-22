43a6fa73ac34bf4dd0af05fb3128485a69aa047c
Peter <peter.tonner@duke.edu>
2016-11-21 19:20:35 -0500

    freezing stuff

--- gpmultipy/freeze.py
+++ gpmultipy/freeze.py
@@ -1,5 +1,16 @@
-import types
-import copy
+import types, copy, json
+import numpy as np
+
+def printSample(sample,depth=1):
+    keys = sample.keys()
+    keys.sort()
+
+    for k in keys:
+        if type(sample[k]) == dict:
+            print "\t".join([""]*depth) + k
+            printSample(sample[k],depth=depth+1)
+        else:
+            print "\t".join([""]*depth) + "%s: %s"%(k,str(sample[k]))
 
 class Freezeable(object):
 
@@ -53,3 +64,18 @@ class Freezer(object):
             ret[k] = v.freeze()
 
         return ret
+
+    def save(self,samples,fname):
+        samples = [copy.deepcopy(s) for s in samples]
+
+        for s in samples:
+            for k,o in self.objects.iteritems():
+                if k in s:
+                    for k2,v in o.freeze().iteritems():
+                        if type(v)==np.ndarray:
+                            s[k][k2] = s[k][k2].tolist()
+
+        s = json.dumps(samples)
+        ofile = open(fname,'w')
+        ofile.write(s)
+        ofile.close()
