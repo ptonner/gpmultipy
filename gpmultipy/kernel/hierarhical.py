15c9f6e8b36bf9fe4dcc0876473ab6c133c27c9e
Peter Tonner <peter.tonner@gmail.com>
2016-12-16 00:06:40 -0500

    add hierarhical

---
+++ gpmultipy/kernel/hierarhical.py
@@ -0,0 +1,33 @@
+from kernel import Kernel
+import numpy as np
+
+class Hierarchical(Kernel):
+
+    def __init__(self,*args,):
+        self.kernels = [a for a in args if issubclass(type(a),Kernel)]
+        self.levels = len(self.kernels)
+
+    def K(self,x,*args,**kwargs):
+
+        ids = x[:,self.levels:]
+        x = x[:,:self.levels]
+
+        # rebuild 2 dims
+        if x.ndim == 1:
+            x = x[:,None]
+
+        k = np.zeros((x.shape[0],x.shape[0]))
+
+        for i,kern in enumerate(self.kernels):
+            kw = {}
+            for k,v in kwargs.iteritems():
+                if k[:3] == 'k%d_'%i:
+                    kw[k[3:]] = v
+
+            for j in np.unique(ids[:,i]):
+                select = np.where(ids[:,i]==j)[0]
+                xselect = x[select,:]
+
+                k[np.ix_(select,select)] += self.kernels[i].K(xselect,**kwargs)
+
+        return k
